54404b172cadce00a1efd270f18163f83ea94746
sabrisadki <sabri.sadki@yahoo.com>
2023-08-29 13:21:34 +0100

    update

---
+++ src/Map.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import GoogleMapReact from 'google-map-react';
+import { Icon } from '@iconify/react';
+import locationIcon from '@iconify/icons-mdi/map-marker';
+
+const LocationPin = ({ text }) => (
+  <div className="pin">
+    <Icon icon={locationIcon} className="pin-icon" />
+    <p className="pin-text">{text}</p>
+  </div>
+);
+
+const Map = ({ location, zoomLevel }) => (
+  <div className="map">
+    <h2 className="map-h2">Come Visit Us At Our Campus</h2>
+
+    <div className="google-map">
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: 'AIzaSyCrLtnH1g-7l63NS4X3FQamtHimNhYn0Ow' }}
+        defaultCenter={location}
+        defaultZoom={zoomLevel}
+      >
+        <LocationPin lat={location.lat} lng={location.lng} text={location.address} />
+      </GoogleMapReact>
+    </div>
+  </div>
+);
+
+export default Map;
